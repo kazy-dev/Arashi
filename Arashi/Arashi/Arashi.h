c26b6ce647e53093c0d6b7c79fb4353ea96c024b
kazy <kazy.dev@gmail.com>
2019-09-05 12:43:10 +0900

    プロジェクトを追加。

---
+++ Arashi/Arashi/Arashi.h
@@ -0,0 +1,19 @@
+//
+//  Arashi.h
+//  Arashi
+//
+//  Created by Yoshikazu on 2019/09/05.
+//  Copyright © 2019 kazy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Arashi.
+FOUNDATION_EXPORT double ArashiVersionNumber;
+
+//! Project version string for Arashi.
+FOUNDATION_EXPORT const unsigned char ArashiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arashi/PublicHeader.h>
+
+
---
+++ Arashi/ArashiTests/ArashiTests.swift
@@ -0,0 +1,34 @@
+//
+//  ArashiTests.swift
+//  ArashiTests
+//
+//  Created by Yoshikazu on 2019/09/05.
+//  Copyright © 2019 kazy. All rights reserved.
+//
+
+import XCTest
+@testable import Arashi
+
+class ArashiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
