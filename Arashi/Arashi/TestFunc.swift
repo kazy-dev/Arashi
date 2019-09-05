7468b40fe61c2c99ff4c3f38aaf774cc99f063ab
kazy <kazy.dev@gmail.com>
2019-09-05 13:04:00 +0900

    テスト用のファイルを追加。

---
+++ Arashi/Arashi/TestFunc.swift
@@ -0,0 +1,15 @@
+//
+//  TestFunc.swift
+//  Arashi
+//
+//  Created by Yoshikazu on 2019/09/05.
+//  Copyright © 2019 kazy. All rights reserved.
+//
+
+import Foundation
+
+public class TestFunc: NSObject {
+    public static func testLog() {
+        print("😄テストだよーん")
+    }
+}
---
+++ Arashi/Example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Yoshikazu on 2019/09/05.
+//  Copyright © 2019 kazy. All rights reserved.
+//
+
+import UIKit
+import Arashi
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TestFunc.testLog()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
